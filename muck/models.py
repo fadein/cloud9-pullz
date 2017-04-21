1b2c4f976909ffb1d370a392df8ed8ad6fe3dc13
Erik Falor <ewfalor@gmail.com>
2017-04-21 04:11:30 +0000

    update mucky model

--- muck/models.py
+++ muck/models.py
@@ -27,10 +27,16 @@ class Person(models.Model):
     suffix = models.CharField(max_length=2, choices=SUFFIXES, default=NOPE )
     dob = models.DateField()
 
+    def __str__(self):
+        return "%s %s %s %s" % (self.first, self.middle, self.last, self.suffix)
+
 class IDTyp(models.Model):
     """Fixed table of acceptable types of identification"""
     abbr = models.CharField('ID abbreviation', max_length = 3)
     desc = models.CharField('ID type description', max_length = 30)
+
+    def __str__(self):
+        return "%s = %s" % (self.abbr, self.desc)
 
 class IDNum(models.Model):
     """ID record affixed to a person"""
@@ -39,6 +45,29 @@ class IDNum(models.Model):
     number = models.CharField('ID number', max_length=40)
     issued = models.DateField('ID number issue date')
 
+    def __str__(self):
+        return "%s = %s" % (self.idtype, self.issued)
+
 class Skill(models.Model):
     person = models.ForeignKey(Person, on_delete=models.CASCADE)
     desc   = models.CharField('Skill description', max_length=50)
+
+    def __str__(self):
+        return "%s = %s" % (self.person, self.desc)
+
+class ContactTyp(models.Model):
+    """Fixed table of acceptable means of contact"""
+    abbr = models.CharField('Contact abbreviation', max_length = 5)
+    desc = models.CharField('Contact description', max_length=50)
+
+    def __str__(self):
+        return "%s = %s" % (self.abbr, self.desc)
+
+class Contact(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    contyp = models.ForeignKey(ContactTyp, on_delete=models.CASCADE)
+    detail = models.CharField('Contact detail', max_length=50)
+
+    def __str__(self):
+        return "(%s) %s = %s" % (self.person, self.contyp, self.detail)
+
